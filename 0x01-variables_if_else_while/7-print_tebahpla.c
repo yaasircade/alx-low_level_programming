0dd2a7c1299ea765615d14abb55812801dd6381b
yaasircade <cadecumar52@gmail>
2023-02-17 15:08:52 -0800

    Loops in C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print letters of the alphabet
+ *
+ * Description: print letters of the alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 122;
+while (1 > 96)
+  {
+    putchar(i);
+    i--;
+  }
+putchar(10);
+return (0);
+}
