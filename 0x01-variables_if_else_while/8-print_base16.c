69e6015062ad0b05102352e967185d92be39ee7c
yaasircade <cadecumar52@gmail>
2023-02-17 16:30:51 -0800

    Loops in C

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int num;
 char letter;
  for (num = 0; num < 10; num++)
    putchar((num % 10) + '0');
- for (letter = 'a'; letter <= 'f'; leter++)
+ for (letter = 'a'; letter <= 'f'; letter++)
    putchar(letter);
 putchar('\n');
 return (0));
